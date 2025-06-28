418b0f78f9e91dbd592dd084fd7ed6524e1c3c93
Caleb Boss <ninjatechdude@gmail.com>
2025-06-28 13:36:39 -0500

    Fixed the App Icon

--- app/root.tsx
+++ app/root.tsx
@@ -29,6 +29,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
+        <link rel="icon" type="image/svg+xml" href="/PokemonWarAndPeaceIcon.png" />
         <Links />
       </head>
       <body>
